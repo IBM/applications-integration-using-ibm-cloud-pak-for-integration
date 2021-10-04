b80ec6de102c2d686ee9795fd132b432cd42bb39
Muralidhar Chavan <muralidhar.chavan@gmail.com>
2021-10-04 17:54:08 +0530

    update script

--- Client/Apps/runClientApps.bat
+++ Client/Apps/runClientApps.bat
@@ -1,9 +1,5 @@
-if "%1" == "" goto no_args
-if "%3" == "" goto args_count_ok
-
-:no_args
-echo Provide ship tracking number
-exit /b 1
+@echo off
+IF [%1]==[] GOTO NOARG
 
 javac -classpath ".\javax.jms-api-2.0.1.jar;.\com.ibm.mq.allclient-9.1.4.0.jar;.\json-20210307.jar" -d . .\com\ibm\mq\samples\jms\*.java
 
@@ -12,3 +8,7 @@ java -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.trustStore=".Client//Cli
 java -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.trustStore=".Client//ClientKey/clientkey.jks" -Djavax.net.ssl.trustStorePassword=%2 -Dcom.ibm.mq.cfg.useIBMCipherMappings=false -classpath ".\javax.jms-api-2.0.1.jar;.\com.ibm.mq.allclient-9.1.4.0.jar;.\json-20210307.jar;." com.ibm.mq.samples.jms.ResponderGetPut
 
 java -Djavax.net.ssl.trustStoreType=jks -Djavax.net.ssl.trustStore=".Client//ClientKey/clientkey.jks" -Djavax.net.ssl.trustStorePassword=%2 -Dcom.ibm.mq.cfg.useIBMCipherMappings=false -classpath ".\javax.jms-api-2.0.1.jar;.\com.ibm.mq.allclient-9.1.4.0.jar;.\json-20210307.jar;." com.ibm.mq.samples.jms.RequestorGET
+
+:NOARG
+@echo Provide Shipment tracking number
+exit /B 1
